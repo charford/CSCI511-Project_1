d8d40ad8c31e83822b43be09ac057359e179606e
Casey Harford <casey@caseyharford.com>
2011-09-25 21:19:06 -0700

    added ParseException class, not fully implemented yet

---
+++ src/ParseException.java
@@ -0,0 +1,13 @@
+/**
+ *  class for ParseException
+ *  provides detailed exceptions for SiteDeveloper
+ *  extends Exception
+*/
+class ParseException extends Exception {
+  /**
+   *  constructor for ParseException
+  */
+  ParseException() {
+    //constructor goes here
+  }
+};
